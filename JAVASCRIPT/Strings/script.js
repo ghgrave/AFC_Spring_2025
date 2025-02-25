91717f50c894e21f9ed5c217076df15df6b5e564
Jon-Mikel <garrisonhgrave@yahoo.com>
2025-02-25 11:54:34 -0600

    added string demo

---
+++ JAVASCRIPT/Strings/script.js
@@ -0,0 +1,12 @@
+// concatenate
+let sentence = "I love " + 'chocolate'
+let age = 22
+console.log(sentence);
+let sentence1 = sentence + " and other stuff"
+console.log(sentence1);
+// template literals
+let sentence2 = `${age > 21 ? "yeah!" : "Boooo"} and other stuff!`
+
+console.log(sentence2);
+
+console.log("Simple test: ", sentence2)
