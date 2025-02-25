32b522dac79dbd0b9fe922dea5d6c967b58deace
Jon-Mikel <garrisonhgrave@yahoo.com>
2025-02-25 10:11:41 -0600

    pushed demo 1

---
+++ JAVASCRIPT/JSDemo1/script.js
@@ -0,0 +1,25 @@
+// console.log("What is your age?")
+// variables
+// var - really should not be using
+// let - focus on - scope-based
+// const - CONSTANT
+
+// let dog = "woof"
+// {
+//   dog = "meow"
+// }
+// console.log(dog);
+
+// dog = "woof"
+// {
+//   let dog = "meow"
+//   console.log("Inside:", dog);
+  
+// }
+// console.log(dog);
+
+// var cat = 12
+// console.log(typeof cat); // determines data type
+// console.log(cat);
+
+
