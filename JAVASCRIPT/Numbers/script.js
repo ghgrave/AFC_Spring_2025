51920e87dba64eebe11bdba74ba726f1ca99b6a6
Jon-Mikel <garrisonhgrave@yahoo.com>
2025-02-25 11:54:20 -0600

    added numbers demo

---
+++ JAVASCRIPT/Numbers/script.js
@@ -0,0 +1,24 @@
+
+// console.log(Number(age));
+// console.log(Number(age2));
+
+// let answer = Number(prompt("What is your age?"))
+// if (typeof answer === "number")
+// console.log(isNaN(answer));
+
+
+// falsey
+// false, null, "", 0 = the same
+// truthy 
+// true, 1 = 1
+
+let count = 1
+// // post-fixing
+// count ++
+// // pre-fixing
+// ++ count
+// count = count + 1
+count += count
+// same above as below
+count = count + count
+console.log(count);
