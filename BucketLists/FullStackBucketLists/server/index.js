738de9655ae2460a9612f1c07c73c509bc76e681
Jon-Mikel <garrisonhgrave@yahoo.com>
2025-04-14 12:07:14 -0500

    added fake data

--- BucketLists/FullStackBucketLists/server/index.js
+++ BucketLists/FullStackBucketLists/server/index.js
@@ -4,17 +4,59 @@
 const express = require("express")
 // create an instance of our application
 const app = express()
+
+// logging tool
 // this will help with routing and params
 const logger = require("morgan")
 app.use(logger("dev"))
 
+// import fake data from mockdata.js file
+const {bucketlist} = require("./mockData");
+
+
 // what "station" (port) are we listening on?
 const port = process.env.PORT || 3000;
 
+// add body parser
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 //HANDLERS
 // verb (route, cb())
-app.get("/", (req, res)=>{
-    res.send("Hello")
+// app.get("/", (req, res)=>{
+//     res.redirect("/home")
+// })
+//
+// app.get("/home", (req, res)=> {
+//     res.send("I am the home page")
+// })
+//
+// // parameters
+// app.get("/api/bucket/:id", (req, res)=>{
+//     console.log(req)
+//     res.send(`Doggy route with parameter: ${req.params.id}`)
+// })
+
+// READ route
+app.get("/api/bucket", (req, res)=>{
+    res.json(bucketlist)
+})
+
+// CREATE data route
+// how is data sent from client
+let newId = 4
+app.post("/api/bucket", (req, res)=>{
+//     what is our goal??
+//     add something to database
+//     new bucket list item
+    let newData = {
+        id: newId++,
+        description: req.body.description,
+        isComplete: false,
+    }
+    bucketlist.push(newData)
+    let receipt = newData
+    res.json(receipt)
 })
 
 //LISTENER
---
+++ BucketLists/FullStackBucketLists/server/mockData.js
@@ -0,0 +1,17 @@
+exports.bucketlist = [
+  {
+    id: 1,
+    description :"Learn Spring Boot",
+    isComplete:  false
+  },
+  {
+    id: 2,
+    description :"Learn React",
+    isComplete:  false
+  },
+  {
+    id: 3,
+    description :"Go to Mexico City",
+    isComplete:  false
+  },
+]
